fafa393a19d90c4345bc937c7d05bca90f1ceca9
bitnahian <nhas9102@uni.sydney.edu.au>
2019-01-06 23:00:17 +0600

    feature/xkcd Modularised code

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
 import settings
 import os
 import discord
-import requests
-
 from discord.ext import commands
-from random import randint
+from modules import xkcd
 
 TOKEN = os.getenv('DISCORD_BOT_API_TOKEN')
 bot = commands.Bot(command_prefix='$', description='Just A Rather Very Intelligent System, now on Discord!')
@@ -27,30 +25,11 @@ async def greet(ctx):
         brief='Retrieves a random xkcd comic')
 async def get_xkcd(ctx):
     try:
-        r = requests.get('http://xkcd.com/info.0.json')
-        data = r.json()
-
-            # Get a random comic between the first and the latest one
-        r = requests.get('http://xkcd.com/%d/info.0.json' % randint(1, data['num']))
-        data = r.json()
-
-        title = data['title']
-        item_url = 'http://xkcd.com/' + str(data['num']) + '/'
-        explanation_url = 'http://explainxkcd.com/' + str(data['num']) + '/'
-        image_url = data['img'].replace('\\', '')
-        subtitle = data['alt']
-
-        embed = discord.Embed(title=title,
-                type='rich',
-                description="{0}\n{1}".format(subtitle, explanation_url),
-                url=item_url,
-                image=image_url).set_image(url=image_url)
-
+        embed=xkcd.process()
         await ctx.send(embed=embed)
 
     except Exception as e:
         print(e)
         await ctx.send("Sorry, something went wrong.")
-
 
 bot.run(TOKEN)
--- modules/xkcd.py
+++ modules/xkcd.py
@@ -1,43 +1,26 @@
 from random import randint
-
+import discord
 import requests
-import requests_cache
 
-def process(input, entities=None):
-    output = {}
-    try:
-        with requests_cache.enabled('xkcd_cache', backend='sqlite', expire_after=3600):
-            # Get the latest comic
-            r = requests.get('http://xkcd.com/info.0.json')
-            data = r.json()
+def process():
+    r = requests.get('http://xkcd.com/info.0.json')
+    data = r.json()
 
-            # Get a random comic between the first and the latest one
-            r = requests.get('http://xkcd.com/%d/info.0.json' % randint(1, data['num']))
-            data = r.json()
+        # Get a random comic between the first and the latest one
+    r = requests.get('http://xkcd.com/%d/info.0.json' % randint(1, data['num']))
+    data = r.json()
 
-        title = data['title']
-        item_url = 'http://xkcd.com/' + str(data['num']) + '/'
-        explanation_url = 'http://explainxkcd.com/' + str(data['num']) + '/'
-        image_url = data['img'].replace('\\', '')
-        subtitle = data['alt']
+    title = data['title']
+    item_url = 'http://xkcd.com/' + str(data['num']) + '/'
+    explanation_url = 'http://explainxkcd.com/' + str(data['num']) + '/'
+    image_url = data['img'].replace('\\', '')
+    subtitle = data['alt']
 
-        buttons = ButtonTemplate()
-        buttons.add_web_url('xkcd Link', item_url)
-        buttons.add_web_url('Explanation Link', explanation_url)
+    embed = discord.Embed(title=title,
+            type='rich',
+            description="{0}\n{1}".format(subtitle, explanation_url),
+            url=item_url,
+            image=image_url).set_image(url=image_url)
 
-        template = GenericTemplate()
-        template.set_image_aspect_ratio_to_square()
-        template.add_element(title=title, item_url=item_url, image_url=image_url, subtitle=subtitle,
-                             buttons=buttons.get_buttons())
+    return embed
 
-        message = template.get_message()
-        message = add_quick_reply(message, 'Check out another!', modules.generate_postback('xkcd'))
-
-        output['input'] = input
-        output['output'] = message
-        output['success'] = True
-    except:
-        error_message = 'There was some error while retrieving data from xkcd.'
-        output['error_msg'] = TextTemplate(error_message).get_message()
-        output['success'] = False
-    return output
