a3a2975f0a115342973ead68227dc9ab542742dc
StylPamp <37444220+StylPamp@users.noreply.github.com>
2019-02-06 13:57:06 +0200

    Delete ping_google.py

--- modules/ping_google.py
+++
@@ -1,9 +0,0 @@
-from os import system
-
-def process():
-    result = system("ping www.google.com")
-    if (result == 0):
-     str = "Google up and running"
-    else:
-     str = "There might be some problem"
-    return str
