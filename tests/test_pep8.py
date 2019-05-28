690b4ce7d068fa99d886d471272381ab5ed2c47d
Sayan Mondal <mondalsayan556@gmail.com>
2019-05-29 02:08:47 +0530

    added on requirements

--- tests/test_pep8.py
+++ tests/test_pep8.py
@@ -7,7 +7,6 @@ class TestCodeFormat(unittest.TestCase):
     def test_pep8_conformance(self):
         """Test that we conform to PEP8. checks all project files"""
         errors = 0
-        style = pep8.StyleGuide(quiet=False)
         style = pycodestyle.StyleGuide(quiet=False)
         style.options.max_line_length = 120
         for root, dirs, files in os.walk("."):
