f6c6343debbd68164ce36aa4e6584b435dd44d27
Sayan Mondal <mondalsayan556@gmail.com>
2019-05-29 03:17:58 +0530

    roll_a_dice added

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from modules import news, image
 
 import discord
 from discord.ext import commands
-from modules import xkcd, flip_a_coin
+from modules import xkcd, flip_a_coin, roll_a_dice
 
 TOKEN = os.getenv('DISCORD_BOT_API_TOKEN')
 bot = commands.Bot(command_prefix='$', description='Just A Rather Very Intelligent System, now on Discord!')
@@ -65,6 +65,21 @@ async def search_image(ctx, search_arg):
 async def flip_coin(ctx):
     try:
         embed = flip_a_coin.coinToss()
+        await ctx.send(embed=embed)
+
+    except Exception as e:
+        print(e)
+        await ctx.send("Sorry, something went wrong.")
+
+
+@bot.command(
+    name="roll_a_dice",
+    description="Roll a dice game",
+    brief="Roll a dice and send result of the head",
+)
+async def roll_dice(ctx):
+    try:
+        embed = roll_a_dice.rollDice()
         await ctx.send(embed=embed)
 
     except Exception as e:
